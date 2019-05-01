6bafcc550a5829a49a4d66b8b0ae4f83aa163f0f
Marc Pascual <marcmpascual@gmail.com>
2019-05-01 23:03:06 +0800

    Create pathogen-setup.sh

---
+++ pathogen-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# http://jens-na.github.io/2013/07/21/pathogen-vim/
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -SLso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
